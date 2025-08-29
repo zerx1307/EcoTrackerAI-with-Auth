8822bf0cdf9a9824f4559bfdbd1b66e48f783af9
23f2000902 <23f2000902@ds.study.iitm.ac.in>
2025-08-29 20:42:40 +0530

    commit

--- app.py
+++ app.py
@@ -51,4 +51,5 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
