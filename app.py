c606b0508b90692462e2edd8fbd83fff3da8c0f9
23f2000902 <23f2000902@ds.study.iitm.ac.in>
2025-09-06 19:03:44 +0530

    cummit

--- app.py
+++ app.py
@@ -52,3 +52,7 @@ app = create_app()
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, debug=True)
+import os
+
+port = int(os.environ.get("PORT", 5000))  # Render sets PORT dynamically
+app.run(host="0.0.0.0", port=port)
